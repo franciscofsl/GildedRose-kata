213fac0406969ba6f1a218244e5c0b3ee5d794cc
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:13:00 +0200

    Add BrieItem testing

---
+++ GildedRoseKata.Tests/BrieItemTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GildedRoseKata.Core;
+
+namespace GildedRoseKata.Tests;
+
+public class BrieItemTest
+{
+    [Fact]
+    public void Should_Update_Brie_Item_Quality_If_Lower_Than_50()
+    {
+        var brieItem = new BrieItem { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+
+        brieItem.UpdateQuality();
+
+        brieItem.Quality.Should().Be(1);
+    }
+
+    [Fact]
+    public void Should_Decrease_SellIn_At_Update_Quality()
+    {
+        var brieItem = new BrieItem { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+
+        brieItem.UpdateQuality();
+
+        brieItem.SellIn.Should().Be(1);
+    }
+
+    [Fact]
+    public void Should_Increase_Quality_If_SellIn_Lower_Than_0()
+    {
+        var brieItem = new BrieItem { Name = "Aged Brie", SellIn = 0, Quality = 5 };
+
+        brieItem.UpdateQuality();
+
+        brieItem.Quality.Should().Be(7);
+    }
+}
