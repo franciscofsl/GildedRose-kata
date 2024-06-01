b3c392135699e220192c50deb11249197b30cd76
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:45:33 +0200

    Add ConjuredItem logic

--- GildedRoseKata.Core/ConjuredItem.cs
+++ GildedRoseKata.Core/ConjuredItem.cs
@@ -2,5 +2,21 @@
 
 public sealed class ConjuredItem : Item
 {
-    
+    public override void UpdateQuality()
+    {
+        if (Quality > 0)
+        {
+            Quality = Quality - 1;
+        }
+
+        SellIn = SellIn - 1;
+
+        if (SellIn < 0)
+        {
+            if (Quality > 0)
+            {
+                Quality = Quality - 1;
+            }
+        }
+    }
 }
