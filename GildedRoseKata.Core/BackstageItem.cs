83fc2e75349ced80b146d5c7cc78534295940d3d
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:20:07 +0200

    Merge BackstageItem conditionals

--- GildedRoseKata.Core/BackstageItem.cs
+++ GildedRoseKata.Core/BackstageItem.cs
@@ -8,20 +8,14 @@ public sealed class BackstageItem : Item
         {
             Quality = Quality + 1;
 
-            if (SellIn < 11)
+            if (SellIn < 11 && Quality < 50)
             {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-                }
+                Quality = Quality + 1;
             }
 
-            if (SellIn < 6)
+            if (SellIn < 6 && Quality < 50)
             {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-                }
+                Quality = Quality + 1;
             }
         }
 
