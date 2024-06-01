c24c2cca01f18bde238d299f326d1b24e11880fd
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 23:07:13 +0200

    Add Quality value object

---
+++ GildedRoseKata.Core/Quality.cs
@@ -0,0 +1,16 @@
+﻿namespace GildedRoseKata.Core;
+
+public sealed record Quality
+{
+    private Quality(int value)
+    {
+        Value = value;
+    }
+
+    internal Quality Of(int value)
+    {
+        return new Quality(value);
+    }
+
+    public int Value { get; }
+}
