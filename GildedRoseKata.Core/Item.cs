ebbdf7dfda625a5ce76ba99b4557110a447e605c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 23:04:20 +0200

    Item as abstract

--- GildedRoseKata.Core/Item.cs
+++ GildedRoseKata.Core/Item.cs
@@ -1,75 +1,11 @@
 ﻿namespace GildedRoseKata.Core
 {
-    public class Item
+    public abstract class Item
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
 
-        public virtual void UpdateQuality()
-        {
-            if (Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (Quality > 0)
-                {
-                    if (Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Quality = Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-
-                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (SellIn < 11)
-                        {
-                            if (Quality < 50)
-                            {
-                                Quality = Quality + 1;
-                            }
-                        }
-
-                        if (SellIn < 6)
-                        {
-                            if (Quality < 50)
-                            {
-                                Quality = Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (Name != "Sulfuras, Hand of Ragnaros")
-            {
-                SellIn = SellIn - 1;
-            }
-
-            if (SellIn < 0)
-            {
-                if (Name != "Aged Brie")
-                {
-                    if (Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Quality > 0)
-                        {
-                            if (Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Quality = Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Quality = Quality - Quality;
-                    }
-                }
-            }
-        }
+        public abstract void UpdateQuality();
     }
 }
