3070040679b0ae078ec3e8e696af8fa46fe15bf8
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:52:22 +0200

    Add ConjuredItem testing

---
+++ GildedRoseKata.Tests/ConjuredItemTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GildedRoseKata.Core;
+
+namespace GildedRoseKata.Tests;
+
+public class ConjuredItemTest
+{
+    [Fact]
+    public void Should_Decrease_Quality_If_Greater_Than_0()
+    {
+        var item = new ConjuredItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
+
+        item.UpdateQuality();
+
+        item.Quality.Should().Be(6);
+    }
+
+    [Fact]
+    public void Should_Decrease_SellIn()
+    {
+        var item = new ConjuredItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 };
+
+        item.UpdateQuality();
+
+        item.SellIn.Should().Be(4);
+    }
+
+    [Fact]
+    public void Should_Decrease_Quality_When_SellIn_Lower_Than_0_And_Quality_Greater_Than_0()
+    {
+        var item = new ConjuredItem { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 20 };
+
+        item.UpdateQuality();
+
+        item.Quality.Should().Be(18);
+    }
+}
