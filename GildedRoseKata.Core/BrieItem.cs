1f016c4c923ad2f617a02e9c9170c4e6518767e1
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:07:56 +0200

    Remove unnecessary logic from BrieItem

--- GildedRoseKata.Core/BrieItem.cs
+++ GildedRoseKata.Core/BrieItem.cs
@@ -4,74 +4,16 @@ public sealed class BrieItem : Item
 {
     public override void UpdateQuality()
     {
-        if (Name != "Aged Brie" && Name != "Backstage passes to a TAFKAL80ETC concert")
+        if (Quality < 50)
         {
-            if (Quality > 0)
-            {
-                if (Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Quality = Quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (Quality < 50)
-            {
-                Quality = Quality + 1;
-
-                if (Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (SellIn < 11)
-                    {
-                        if (Quality < 50)
-                        {
-                            Quality = Quality + 1;
-                        }
-                    }
-
-                    if (SellIn < 6)
-                    {
-                        if (Quality < 50)
-                        {
-                            Quality = Quality + 1;
-                        }
-                    }
-                }
-            }
+            Quality++;
         }
 
-        if (Name != "Sulfuras, Hand of Ragnaros")
-        {
-            SellIn = SellIn - 1;
-        }
+        SellIn--;
 
-        if (SellIn < 0)
+        if (SellIn < 0 && Quality < 50)
         {
-            if (Name != "Aged Brie")
-            {
-                if (Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Quality > 0)
-                    {
-                        if (Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Quality = Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    Quality = Quality - Quality;
-                }
-            }
-            else
-            {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-                }
-            }
+            Quality++;
         }
     }
 }
