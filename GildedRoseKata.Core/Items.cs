ed5a82778bf6faa9ea169d810ae2392d8f8ec286
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 23:29:16 +0200

    Add ItemsCollection

---
+++ GildedRoseKata.Core/Items.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace GildedRoseKata.Core;
+
+public class ItemsCollection : IEnumerable<Item>
+{
+    private readonly IList<Item> _items = new List<Item>();
+
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(BrieItem item)
+    {
+        _items.Add(item);
+    }
+
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < _items.Count; i++)
+        {
+            var item = _items[i];
+
+            item.UpdateQuality();
+        }
+    }
+}
---
+++ GildedRoseKata.Tests/Items.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using GildedRoseKata.Core;
+
+namespace GildedRoseKata.Tests;
+
+public class ItemsCollectionTest
+{
+    [Fact]
+    public void Should_Update_Items_Quality()
+    {
+        var originalQuality = Quality.Default;
+
+        var item = new BrieItem { SellIn = 2, Quality = originalQuality };
+
+        var items = new ItemsCollection
+        {
+            item
+        };
+
+        items.UpdateQuality();
+
+        item.Quality.Should().NotBe(originalQuality);
+    }
+}
