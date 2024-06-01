8a9d7223ba518340464d510312c97062f8796007
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-06-01 22:37:25 +0200

    Add SulfurasItem testing

---
+++ GildedRoseKata.Tests/SulfurasItemTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using GildedRoseKata.Core;
+
+namespace GildedRoseKata.Tests;
+
+public class SulfurasItemTest
+{
+    [Fact]
+    public void Should_Increase_Quality_If_Lower_Than_50()
+    {
+        var item = new SulfurasItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 10 };
+
+        item.UpdateQuality();
+
+        item.Quality.Should().Be(11);
+    }
+}
